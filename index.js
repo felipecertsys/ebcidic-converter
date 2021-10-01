c21a86982aef76e45a5bd189f39e896e0d3c7236
Felipe Amaral <felipe.souza@certsys.com.br>
2021-10-01 16:24:20 -0300

    Adição de primeira versão de encrypt e decrypt

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const EBCDIC = require('nm-ebcdic-converter');
 var encoding = require("encoding");
 
 var chave = "Batata123";
-
 // var utfToAscii = encoding.convert("Batata123", 'UTF-8', 'ASCII');
 // var asciiToUtf = encoding.convert("a", 'ASCII', 'UTF-8');
 var asciiToEbcdic = EBCDIC.fromASCII("F");
