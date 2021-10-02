29564506b561fa90ef8fa1dfd06c3151115ae0b0
Felipe Amaral de Souza <amaral.felipe@protonmail.com>
2021-10-02 10:44:53 -0300

    Adicionado métodos de encrypt e decrypt e dockerfile

---
+++ app/index.js
@@ -0,0 +1,22 @@
+const EBCDIC = require('nm-ebcdic-converter');
+
+let chave = "Batata123";
+
+let arrChave1 = [];
+
+chave.split('').forEach(x => {
+    var asciiToEbcdic = EBCDIC.fromASCII(x);
+    arrChave1.push(asciiToEbcdic);
+});
+
+console.log(arrChave1);
+
+let arrChave2 = [];
+
+arrChave1.forEach(x => {
+    var ebcdicToAscii = EBCDIC.toASCII(x)
+    arrChave2.push(ebcdicToAscii)
+})
+
+let string = arrChave2.join('');
+console.log(string);
